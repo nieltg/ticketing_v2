95fc3cf30825308aa2de11c29e5fb26bdb1c9ba4
Daniel <nieltansah@gmail.com>
2019-01-24 18:05:52 +0700

    api: Add serializers for redeem-purchase/ endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -3,9 +3,23 @@ from rest_framework import serializers
 from .models import Ticket, TicketPurchase
 
 
+class TicketPurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TicketPurchase
+        fields = (
+            'name',
+            'email',
+            'phone',
+        )
+
+
 class RedeemRequestSerializer(serializers.Serializer):
     ticket = serializers.SlugRelatedField(
         slug_field='code', queryset=Ticket.objects.all())
+
+
+class RedeemPurchaseRequestSerializer(RedeemRequestSerializer):
+    ticket_purchase = TicketPurchaseSerializer()
 
 
 class RedeemResponseSerializer(serializers.ModelSerializer):
