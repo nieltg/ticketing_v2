d97726de6cf6c24bc7dc2a797859c01c11196978
Daniel <nieltansah@gmail.com>
2019-01-24 12:02:09 +0700

    api: Add Ticket, TicketPurchase, SouvenirRedeem model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    code = models.CharField(
+        max_length=13, blank=False, null=False, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class TicketPurchase(models.Model):
+    ticket = models.OneToOneField(
+        Ticket, on_delete=models.PROTECT, primary_key=True)
+
+    name = models.TextField(blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    phone = models.TextField(blank=False, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class SouvenirRedeem(models.Model):
+    ticket_purchase = models.OneToOneField(
+        TicketPurchase, on_delete=models.PROTECT, primary_key=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
